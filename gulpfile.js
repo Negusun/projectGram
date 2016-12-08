3fb74548a32956f428f365ae4927f331dd6716ff
negusun <ngprodevr@gmail.com>
2016-12-08 20:38:48 -0300

    mas dependencias para la tarea styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
+/**
+ * Tareas para el hacer el bundle
+ */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean-css');
 
 gulp.task('styles', function(){
   gulp.src('index.scss')
     .pipe(sass())
+    .pipe(clean({compatibility: 'ie8'}))
+    .pipe(rename('app.css'))
     .pipe(gulp.dest('public'));
 });
 
