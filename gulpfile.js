1e5d2be46b9d6b3c2815cb9c35e759b25d6a4861
negusun <ngprodevr@gmail.com>
2016-12-08 22:17:08 -0300

    agregada la task del bundle js + browserify + babelify + vinyl-source

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean-css');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function(){
   gulp.src('index.scss')
@@ -19,4 +22,13 @@ gulp.task('assets', function(){
   .pipe(gulp.dest('public'));
 });
 
-gulp.task('default', ['styles', 'assets']);
+gulp.task('scripts', function(){
+  browserify('./src/index.js')
+    .transform(babel)//para que los navegadores soporten ECMAScript6
+    .bundle()//genera el bundle de browserify
+    .pipe(source('index.js'))//con viny-source se transforma a un archivo que entiende gulp
+    .pipe(rename('app.js'))//se renombra el archivo
+    .pipe(gulp.dest('public'));//se envia a el destino en public
+});
+
+gulp.task('default', ['styles', 'assets', 'scripts']);
